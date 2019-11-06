d58f38816f25787c54429b82b691a707dbb254c4
jervicBad <jervic.badillo@gmail.com>
2019-11-05 20:56:56 -0500

    revert font

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <!--JB -->
-  <link href="https://fonts.googleapis.com/css?family=Lexend+Exa&display=swap" rel="stylesheet">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Lexend+Exa&display=swap" rel="stylesheet"> -->
 </head>
 
 
